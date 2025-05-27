3c1a2e64bdda2f0a2296fd0ba8f7717361e009a6
fDero <francescodero@outlook.it>
2025-05-27 19:46:28 +0200

    Add more test cases for `persistence/disk`

--- persistence/disk_test.go
+++ persistence/disk_test.go
@@ -52,3 +52,35 @@ func TestWritingAndReadingMultipleElements(t *testing.T) {
 		t.Errorf("Expected %d elements in the output, got %d elements", len(input), len(output))
 	}
 }
+
+func TestWritingAndReadingMultipleElementsExtended(t *testing.T) {
+
+	bigNumber := uint64(12345678901234567890)
+	bigDocId := core.DocumentId(bigNumber)
+	bigPosition := core.TermPosition(bigNumber)
+
+	if enc := vbyteEncodeUInt64(bigNumber); len(enc) <= 1 {
+		t.Fatalf("we want `bigNumber` to have a large encoding in variable length: %v", bigNumber)
+	}
+
+	input := []core.TermTracker{
+		{DocId: 1, Position: 2},
+		{DocId: 1, Position: 3},
+		{DocId: 2, Position: 4},
+		{DocId: 2, Position: 5},
+		{DocId: 2, Position: bigPosition},
+		{DocId: 3, Position: 7},
+		{DocId: 3, Position: 8},
+		{DocId: bigDocId, Position: 1},
+		{DocId: bigDocId, Position: 2},
+	}
+
+	buffer := new(bytes.Buffer)
+	inputIterator := data.NewSliceIterator(input)
+	encodeTermTrackersToDisk(buffer, inputIterator)
+	output := data.CollectAsSlice(iterateTermTrackersFromDisk(buffer))
+
+	if len(output) != len(input) {
+		t.Errorf("Expected %d elements in the output, got %d elements", len(input), len(output))
+	}
+}
