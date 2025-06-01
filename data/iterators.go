16592b7cc6952b57ce1f7fafd3f537969f40ed98
fDero <francescodero@outlook.it>
2025-06-01 23:59:34 +0200

    Implement some `iter.Seq2` based utilities

--- data/iterators.go
+++ data/iterators.go
@@ -20,6 +20,31 @@ import (
 	"strings"
 )
 
+func ZipIterators[T, U any](firstIterator iter.Seq[T], secondIterator iter.Seq[U]) iter.Seq2[T, U] {
+	nextFirst, stopFirst := iter.Pull(firstIterator)
+	nextSecond, stopSecond := iter.Pull(secondIterator)
+	return func(yield func(T, U) bool) {
+		firstElem, firstExists := nextFirst()
+		secondElem, secondExists := nextSecond()
+		for firstExists && secondExists {
+			if !yield(firstElem, secondElem) {
+				break
+			}
+			firstElem, firstExists = nextFirst()
+			secondElem, secondExists = nextSecond()
+		}
+		stopFirst()
+		stopSecond()
+	}
+}
+
+func ZipSlices[T, U any](firstSlice []T, secondSlice []U) iter.Seq2[T, U] {
+	return ZipIterators(
+		NewSliceIterator(firstSlice),
+		NewSliceIterator(secondSlice),
+	)
+}
+
 func CollectAsSlice[T any](seq iter.Seq[T]) []T {
 	var result []T
 	for value := range seq {
